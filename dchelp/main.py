98522f597dfeb4f125707bd347e7e497ab25944f
smokehill <valera.padolochniy@gmail.com>
2021-02-23 13:40:32 +0300

    custom help

--- dchelp/main.py
+++ dchelp/main.py
@@ -13,8 +13,10 @@ from dchelp import DCHelp
 parser = argparse.ArgumentParser(
     prog='dchelp',
     formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=30),
-    description='wrappers for docker-compose'
+    description='wrappers for docker-compose',
+    add_help=False
 )
+parser.add_argument('--help', help="show this help message", action="store_true")
 parser.add_argument('--stat', help="common statistics", action="store_true")
 parser.add_argument('--list', help="projects list", action="store_true")
 parser.add_argument('--live', help="projects list (live mode)", action="store_true")
@@ -28,30 +30,25 @@ signal.signal(signal.SIGINT, lambda x,y: sys.exit(0))
 dchelp = DCHelp()
 
 def main():
-    if args.stat:
+    if args.help:
+        parser.print_help()
+    elif args.stat:
         dchelp.stat()
-
     elif args.list:
         dchelp.list()
-
     elif args.live:
         dchelp.live()
-
     elif args.up:
         dchelp.up()
-
     elif args.down:
         dchelp.down()
-
     elif args.reset:
         dchelp.reset()
-
     elif args.test:
         suite = unittest.TestLoader().loadTestsFromModule(tests)
         unittest.TextTestRunner().run(suite)
-
     else:
-        print("use [-h] for help")
+        print('use: dchelp --help')
 
 
 if __name__ == '__main__':
